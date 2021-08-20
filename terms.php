f1f6ee1d8071b5404efb035bc5aad34d68f796fd
Idifavour <idifavour7@gmail.com>
2021-08-20 20:33:53 +0100

    Added

---
+++ terms.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">
+    <link href="http://fonts.cdnfonts.com/css/adobe-clean" rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Terms</title>
+</head>
+<body>
+    <header>
+        <?php include_once "Components/navbar.php";  ?>
+    </header>
+    <main class="faq">
+        <section class="preview d-flex flex-column justify-content-center align-items-center">
+            <h2 class="text-white">Terms of Services</h2>
+        </section>
+        <div class="container-fluid mt-5 mb-5">
+            <div class="container">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-md-10 bg-white border-3 rounded-2 col-lg-10 col-xl-9 p-4">
+                        <h2 class="h4"><strong>1</strong> TERMS ONE</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur ut magni, tempore ipsum iusto molestiae voluptate placeat corporis impedit porro, illo quidem magnam distinctio blanditiis eaque deserunt beatae quas. Vel.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates cupiditate molestias cumque est optio repellat earum voluptatem ex iste provident quaerat dicta quasi nulla, aliquam quidem nesciunt dolore culpa quam.
+                        </p>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe quos enim minima assumenda a voluptatum, ratione nihil delectus porro minus quisquam eum non corporis optio? Ducimus corrupti facere nostrum deserunt!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quod beatae ipsam distinctio? Doloribus provident pariatur aperiam dolorem ex unde vitae nulla, in illum deserunt repellat non ipsa doloremque delectus.
+                        </p>
+                        <h2 class="h4 mt-4"><strong>2</strong> TERMS TWO</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur ut magni, tempore ipsum iusto molestiae voluptate placeat corporis impedit porro, illo quidem magnam distinctio blanditiis eaque deserunt beatae quas. Vel.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates cupiditate molestias cumque est optio repellat earum voluptatem ex iste provident quaerat dicta quasi nulla, aliquam quidem nesciunt dolore culpa quam.
+                        </p>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe quos enim minima assumenda a voluptatum, ratione nihil delectus porro minus quisquam eum non corporis optio? Ducimus corrupti facere nostrum deserunt!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quod beatae ipsam distinctio? Doloribus provident pariatur aperiam dolorem ex unde vitae nulla, in illum deserunt repellat non ipsa doloremque delectus.
+                        </p>
+                        <h2 class="h4 mt-4"><strong>3</strong> TERMS THREE</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur ut magni, tempore ipsum iusto molestiae voluptate placeat corporis impedit porro, illo quidem magnam distinctio blanditiis eaque deserunt beatae quas. Vel.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates cupiditate molestias cumque est optio repellat earum voluptatem ex iste provident quaerat dicta quasi nulla, aliquam quidem nesciunt dolore culpa quam.
+                        </p>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe quos enim minima assumenda a voluptatum, ratione nihil delectus porro minus quisquam eum non corporis optio? Ducimus corrupti facere nostrum deserunt!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quod beatae ipsam distinctio? Doloribus provident pariatur aperiam dolorem ex unde vitae nulla, in illum deserunt repellat non ipsa doloremque delectus.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    
+    <!-- Footer -->
+    <?php include_once "Components/footer.php" ?>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.js"></script>
+
+</body>
+
+</html>
